8ac7be36055586a57a96fa9a2c68f564abbafc8b
omajork <126159210+omajork@users.noreply.github.com>
2023-02-23 16:20:07 +0100

    Create main-head.php

---
+++ main-head.php
@@ -0,0 +1,58 @@
+
+            <header id="masthead" class="site-header header-v2">
+                <div class="container hidden-md-down">
+                    <div class="row">
+
+                        <!-- ============================================================= Header Logo ============================================================= -->
+                        <div class="header-logo">
+                            <a href="./" class="header-logo-link">
+                                 <img src="./assets/images/logo.png">  
+                            </a>
+                        </div>
+                        <!-- ============================================================= Header Logo : End============================================================= -->
+
+                        <div class="primary-nav animate-dropdown">
+                            <div class="clearfix">
+                                 <button class="navbar-toggler hidden-sm-up pull-right flip" type="button" data-toggle="collapse" data-target="#default-header">
+                                        &#9776;
+                                 </button>
+                            </div>
+
+                            <!--<div class="collapse navbar-toggleable-xs" id="default-header">
+                                <nav>
+                                    <ul id="menu-main-menu" class="nav nav-inline yamm">
+                                        <li class="menu-item animate-dropdown"><a title="Home" href="index.php">Home</a>
+                                        </li>
+                                        <li class="highlight menu-item animate-dropdown"><a title="Super Deals" href="#">Super Deals</a></li>
+                                        <li class="menu-item animate-dropdown"><a title="Featured" href="#">Featured Brands</a></li>
+
+                                        <li class="menu-item menu-item-has-children animate-dropdown dropdown"><a title="Abamade & More" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true">Abamade & More</a>
+                                            <ul role="menu" class=" dropdown-menu">
+                                                <li class="menu-item animate-dropdown"><a title="Blogs" href="blogs.php">Our Blogs</a></li>
+                                                <li class="menu-item animate-dropdown"><a title="About Us" href="about.php">About Us</a></li>
+                                                <li class="menu-item animate-dropdown"><a title="Our Policy" href="policy.php">Our Policy</a></li>
+                                                <li class="menu-item animate-dropdown"><a title="Contact Us" href="contact.php">Contact Us</a></li>
+                                            </ul>
+                                        </li>
+                                        
+                                    </ul>
+                                </nav>
+                            </div>-->
+                        </div>
+
+                        <div class="header-support-info">
+                            <div class="media">
+                                <span class="media-left support-icon media-middle"><i class="ec ec-support"></i></span>
+                                <div class="media-body">
+                                    <span class="support-number"><strong>Support</strong> (234) 81-7962-5504</span><br/>
+                                    <span class="support-email"> info@abamade.com.ng</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div><!-- /.row -->
+                </div>
+
+                        <?php include_once 'mobile-menu.php'; ?>
+                 
+            </header><!-- #masthead -->
