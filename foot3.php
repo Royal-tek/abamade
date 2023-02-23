cede7139646cb9bd56c850e25d7cb7f38d488ed4
omajork <126159210+omajork@users.noreply.github.com>
2023-02-23 16:20:30 +0100

    Create foot3.php

---
+++ foot3.php
@@ -0,0 +1,139 @@
+   <footer id="colophon" class="site-footer">
+            	            	<div class="footer-newsletter">
+            		<div class="container">
+            			<div class="row">
+            				<div class="col-xs-12 col-sm-12">
+            					<h5 class="newsletter-title">Sign up as our first 100 Customer</h5>
+            					<span class="newsletter-marketing-text">...and receive <strong>20% discount for first shopping</strong></span>
+            				</div>
+            				<!--<div class="col-xs-12 col-sm-5">
+            					<form>
+            						<div class="input-group">
+            							<input type="text" class="form-control" placeholder="Enter your email address">
+            							<span class="input-group-btn">
+            								<button class="btn btn-secondary" type="button">Sign Up</button>
+            							</span>
+            						</div>
+            					</form>
+            				</div>-->
+            			</div>
+            		</div>
+            	</div>
+
+            	<div class="footer-bottom-widgets">
+            		<div class="container">
+            			<div class="row">
+            				<div class="col-xs-12 col-sm-12 col-md-7 col-md-push-5">
+            					<div class="columns">
+            						<aside id="nav_menu-2" class="widget clearfix widget_nav_menu">
+            							<div class="body">
+            								<h4 class="widget-title">Know Us Better</h4>
+            								<div class="menu-footer-menu-1-container">
+            									<ul id="menu-footer-menu-1" class="menu">
+            										<li class="menu-item"><a href="about.php"> About Us</a></li>
+            										<li class="menu-item"><a href="contact.php"> Contact Us</a></li>
+            										<li class="menu-item"><a href="policy.php">Privacy Policy </a></li>
+            										<li class="menu-item"><a href="terms.pdf" target="_blank">Terms  &#038; Conditions</a></li>
+            										
+            									</ul>
+            								</div>
+            							</div>
+            						</aside>
+            					</div><!-- /.columns -->
+
+            					<div class="columns">
+            						<aside id="nav_menu-3" class="widget clearfix widget_nav_menu">
+            							<div class="body">
+            								<h4 class="widget-title">&nbsp;</h4>
+            								<div class="menu-footer-menu-2-container">
+            									<ul id="menu-footer-menu-2" class="menu">            										
+												    <li class="menu-item"><a href="https://vendors.abamade.com.ng">Be a Vendor</a></li>
+            										<li class="menu-item"><a href="https://couriers.abamade.com.ng">Be a Courier</a></li>
+            										<li class="menu-item"><a href="#">FAQs</a></li>
+            										<li class="menu-item"><a href="./contact.php">Product Support</a></li>
+            									</ul>
+            								</div>
+            							</div>
+            						</aside>
+            					</div><!-- /.columns -->
+
+            					<div class="columns">
+            						<aside id="nav_menu-4" class="widget clearfix widget_nav_menu">
+            							<div class="body">
+            								<h4 class="widget-title">My Access Care</h4>
+            								<div class="menu-footer-menu-3-container">
+            									<ul id="menu-footer-menu-3" class="menu">
+            										<li class="menu-item"><a href="my-account.php">My Account</a></li>
+            										<li class="menu-item"><a href="#">Track your Order</a></li>
+            										<li class="menu-item"><a href="wishlist.php">Wishlist</a></li>
+            										<li class="menu-item"><a href="cart.php">My Cart</a></li>
+            									</ul>
+            								</div>
+            							</div>
+            						</aside>
+            					</div><!-- /.columns -->
+
+            				</div><!-- /.col -->
+
+            				<div class="footer-contact col-xs-12 col-sm-12 col-md-5 col-md-pull-7">
+            					<div class="footer-logo">
+            						<img src="./assets/images/logo.png">
+            					</div><!-- /.footer-contact -->
+
+            					<div class="footer-call-us">
+            						<div class="media">
+            							<p>We are a group of marketers showcasing Nigeria's finest products to Nigerians and foreigners at home and in the diaspora.</p>
+            							<strong>Address:</strong> 32 Hospital Road, Port Harcourt.<br/>
+            							<strong>Mobile:</strong> +(234) 81-7962-5504<br/>
+            							<strong>Email:</strong> info@abamade.com.ng<br/>
+
+            							<!--	<span class="media-left call-us-icon media-middle"><i class="ec ec-support"></i></span>
+            							<div class="media-body">
+            								<span class="call-us-text">Got Questions ? Call us 24/7!</span>
+            								<span class="call-us-number">(234) 90-2985-5195, (234) 81-7962-5504</span>
+            							</div>-->
+            						</div>
+            					</div><!-- /.footer-call-us -->
+
+<!-- /.footer-address
+            					<div class="footer-address">
+            						
+            						<address>17 Princess Road, Aba, Abia State, Nigeria.</address>
+            					</div> 
+
+            					<div class="footer-social-icons">
+            						<ul class="social-icons list-unstyled">
+            							<li><a class="fa fa-facebook" href="#"></a></li>
+            							<li><a class="fa fa-twitter" href="#"></a></li>
+            							<li><a class="fa fa-pinterest" href="#"></a></li>
+            							<li><a class="fa fa-linkedin" href="#"></a></li>
+            							<li><a class="fa fa-google-plus" href="#"></a></li>
+            							<li><a class="fa fa-tumblr" href="#"></a></li>
+            							<li><a class="fa fa-instagram" href="#"></a></li>
+            							<li><a class="fa fa-youtube" href="#"></a></li>
+            							<li><a class="fa fa-rss" href="#"></a></li>
+            							</ul>
+            					</div>-->
+            				</div>
+
+            			</div>
+            		</div>
+            	</div>
+
+            	<div class="copyright-bar">
+            		<div class="container">
+            			<div class="pull-left flip copyright">&copy; <a href="#">Abamade</a> <?php echo date('Y', time()); ?> - All Rights Reserved</div>
+            			<div class="pull-right flip payment">
+            				<div class="footer-payment-logo">
+            					<ul class="cash-card card-inline">
+            						<li class="card-item"><img src="assets/images/footer/payment-icon/1.png" alt="" width="52"></li>
+            						<li class="card-item"><img src="assets/images/footer/payment-icon/2.png" alt="" width="52"></li>
+            						<li class="card-item"><img src="assets/images/footer/payment-icon/3.png" alt="" width="52"></li>
+            						<li class="card-item"><img src="assets/images/footer/payment-icon/4.png" alt="" width="52"></li>
+            						<li class="card-item"><img src="assets/images/footer/payment-icon/5.png" alt="" width="52"></li>
+            					</ul>
+            				</div><!-- /.payment-methods -->
+            			</div>
+            		</div><!-- /.container -->
+            	</div><!-- /.copyright-bar -->
+            </footer><!-- #colophon -->
