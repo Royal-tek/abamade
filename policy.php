a92df0967132377b2a475b4de952be49b88b0330
omajork <126159210+omajork@users.noreply.github.com>
2023-02-23 14:14:44 +0100

    Create policy.php

---
+++ policy.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+   <title>ABAMADE eCommerce Platform</title>
+    <meta name="description" content="Made by Nigerians for Nigerians and the World.">
+    <meta name="author" content="TACOEE CONSULTS LIMITED - https://tacoee.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- ::::::::::::::Favicon icon::::::::::::::-->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/png">
+
+    <!-- ::::::::::::::All CSS Files here :::::::::::::: -->
+
+    <!-- Vendor CSS Files -->
+    <link rel="stylesheet" href="assets/css/vendor/jquery-ui.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/vendor/plaza-icon.css">
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+
+    <!-- Plugin CSS Files -->
+    <link rel="stylesheet" href="assets/css/plugin/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/plugin/material-scrolltop.css">
+    <link rel="stylesheet" href="assets/css/plugin/price_range_style.css">
+    <link rel="stylesheet" href="assets/css/plugin/in-number.css">
+    <link rel="stylesheet" href="assets/css/plugin/venobox.min.css">
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <!-- <link rel="stylesheet" href="assets/css/vendor/vendor.min.css"/>
+    <link rel="stylesheet" href="assets/css/plugin/plugins.min.css"/>
+    <link rel="stylesheet" href="assets/css/main.min.css"> -->
+
+    <!-- Main Style CSS File -->
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+
+    <!-- ::::::  Start  Header Section  ::::::  -->
+     <?php
+            include_once 'head.php';
+        ?><!-- ::::::  End  Header Section  ::::::  -->
+
+   <!-- ::::::  Start  Breadcrumb Section  ::::::  -->
+    <div class="page-breadcrumb">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <ul class="page-breadcrumb__menu">
+                        <li class="page-breadcrumb__nav"><a href="./index.php">Home</a></li>
+                        <li class="page-breadcrumb__nav active">Privacy Policy</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div> <!-- ::::::  End  Breadcrumb Section  ::::::  -->
+
+    <!-- ::::::  Start  Main Container Section  ::::::  -->
+    <main id="main-container" class="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="about-top m-b-30">
+                        <div class="row">
+                            <!--<div class="col-lg-6">
+                                <div class="about-img m-b-30">
+                                    <div class="img-responsive m-b-40">
+                                        <img src="assets/img/about/about-1.jpg" alt="">
+                                    </div>
+                                </div>
+                            </div>-->
+                            <div class="col-lg-12">
+                                <div class="about-content">
+                                    <h1 class="title title--large title--thin">Privacy Policy</h1>
+                                    <div class="para__content">
+                                        <p class="para__text">
+                                            <h5>1. About this Notice</h5>
+                                         
+                                            Tick Technologies built the Abamade app as a service based app. This SERVICE is provided by Tick Technologies at a minimal cost and is intended for use as is. If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.<br/>
+                                            The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at our website (https://abamade.com.ng) unless otherwise defined in this Privacy Policy.<br/>
+                                            This page thus is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service. This Privacy and Cookie Notice provides information on how Abamade collects and processes your personal data when you visit our website or mobile applications.
+                                        </p>
+                                        <p class="para__text"><h5>2. The Data We Collect About You?</h5>
+                                            We collect your personal data in order to provide and continually improve our products and services. We may collect, use, store and transfer different kinds of personal data about you:  for marketing and personal data optimization purposes. Abamade also uses Google Digital Marketing to propose targeted offers.To find out more:
+                                            <ol>
+                                                <li>a. Information you provide to us: We receive and store the information you provide to us including your identity data, contact data, delivery address and financial data.</li>
+                                                <li>b. Information on your use of our website and/or mobile applications: We automatically collect and store certain types of information regarding your use of the Abamade platform including information about your searches, views, downloads and purchases.</li>
+                                                <li>c. Information from third parties and publicly available sources: We may receive information about you from third parties including our carriers; payment service providers; merchants/brands; and advertising service providers.
+                                                </li>
+                                            </ol>
+
+                                        </p>
+                                        <p class="para__text"><h5>3. Cookies and How We Use Them</h5>
+                                            A cookie is a small file of letters and numbers that we put on your computer if you agree.
+                                            Cookies allow us to distinguish you from other users of our website and mobile applications, which helps us to provide you with an enhanced browsing experience. For example we use cookies for the following purposes:
+                                            <ol>
+                                                <li>a. Recognizing and counting the number of visitors and to see how visitors move around the site when they are using it (this helps us to improve the way our website works, for example by ensuring that users can find what they are looking for).</li>
+                                                <li>b. Identifying your preferences and subscriptions e.g. language settings, saved items, items stored in your basket and Prime membership; and</li>
+                                                <li>c. Sending you newsletters and commercial/advertising messages tailored to your interests.</li>
+                                            </ol>
+                                            <br/>
+                                            Our approved third parties may also set cookies when you use our platforms. Third parties include search engines, providers of measurement and analytics services, social media networks and advertising companies.
+                                        </p>
+                                        <p class="para__text"><h5>4. How We Use Your Personal Data</h5>
+                                            We use your personal data to operate, provide, develop and improve the products and services that we offer, including the following:<br/>
+                                            •   Registering you as a new customer/vendor/courier.<br/>
+                                            •   Processing and delivering your orders.<br/>
+                                            •   Managing your relationship with us.<br/>
+                                            •   Enabling you to participate in promotions, competitions and surveys.<br/>
+                                            •   Improving our website, applications, products and services<br/>
+                                            •   Recommending/advertising products or services which may be of interest to you.<br/>
+                                            •   Complying with our legal obligations, including verifying your identity where necessary.<br/>
+                                            •   Optimizing the service you offer to other customers through our platforms.<br/>
+                                            •   Detecting fraud.
+
+                                        </p>
+                                        <p class="para__text"><h5>5. How We Share Your Personal Data</h5>
+                                            We may need to share your personal data with third parties for the following purposes:<br/>
+                                            •   Sale of products and services: In order to deliver your products and services purchased on our platform from third parties, we may be required to provide your personal data to such third parties.<br/>
+                                            •   Working with third party service providers: We engage third parties to perform certain functions on our behalf. Examples include fulfilling orders for products or services, delivering packages, analyzing data, providing marketing assistance, processing payments, transmitting content, assessing and managing credit risk, and providing customer service.<br/>
+                                            •   Business transfers: As we continue to develop our business, we might sell or buy other businesses or services. In such transactions, customer information may be transferred together with other business assets.<br/>
+                                            •   Detecting fraud and abuse: We release account and other personal data to other companies and organizations for fraud protection and credit risk reduction, and to comply with the law.
+                                            When we share your personal data with third parties we:<br/>
+                                            •   require them to agree to use your data in accordance with the terms of our Privacy Policy, Terms & Conditions and in accordance with the law; and<br/>
+                                            •   only permit them to process your personal data for specified purposes and in accordance with our instructions. We do not allow our third-party service providers to use your personal data for their own purposes.
+
+                                        </p>
+                                        <p class="para__text"><h5>6. International Transfers</h5>
+                                            We may transfer your personal data to locations in another country, if this is permissible pursuant to applicable laws in your location. There are inherent risks in such transfers.<br/>
+                                            In the event of international transfers of your personal data, we shall put in place measures necessary to protect your data, and we shall continue to respect your legal rights pursuant to the terms of this Privacy and Cookie Notice and applicable laws in your location.
+
+                                        </p>
+                                        <p class="para__text"><h5>7. Data Security</h5>
+                                            We have put in place appropriate security measures to prevent your personal data from being accidentally lost, used or accessed in an unauthorized way, altered or disclosed.<br/>
+                                            In addition, we limit access to your personal data to those employees, agents, contractors and other third parties who have a business need to know. They will only process your personal data on our instructions and they are subject to a duty of confidentiality.
+                                            We have put in place procedures to deal with any suspected personal data breach and will notify you and any applicable regulator of a breach where we are legally required to do so.
+
+                                        </p>
+                                         <p class="para__text"><h5>8. Your Legal Rights</h5>
+                                            It is important that the personal data we hold about you is accurate and current. Please keep us informed if your personal data changes during your relationship with us.
+                                            Under certain circumstances, you have rights under data protection laws in relation to your personal data, including the right to access, correct or erase your personal data, object to or restrict processing of your personal data, and unsubscribe from our emails and newsletters.
+
+                                         </p>
+                                         <p class="para__text"><h5>9. Children’s Privacy</h5>
+                                            These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.
+                                         </p>
+                                         <p class="para__text"><h5>10. Changes to This Privacy Policy</h5>
+                                            We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.
+                                         </p>
+                                         <p class="para__text"><h5>11. Further Details</h5>
+                                            If you are looking for more information on how we process your personal data, or you wish to exercise your legal rights in respect of your personal data, please contact info@abamade.com.ng.
+                                         </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    </main> <!-- ::::::  End  Main Container Section  ::::::  -->
+
+    <!-- ::::::  Start  Footer Section  ::::::  -->
+      <?php
+            include_once 'foot.php';
+        ?><!-- ::::::  End  Footer Section  ::::::  -->
+
+    <!-- material-scrolltop button -->
+    <button class="material-scrolltop" type="button"></button>
+    
+
+    <!-- ::::::::::::::All Javascripts Files here ::::::::::::::-->
+
+    <!-- Vendor JS Files -->
+    <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+    <script src="assets/js/vendor/jquery-ui.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.js"></script>
+
+    <!-- Plugins JS Files -->
+    <script src="assets/js/plugin/swiper.min.js"></script>
+    <script src="assets/js/plugin/jquery.countdown.min.js"></script>
+    <script src="assets/js/plugin/material-scrolltop.js"></script>
+    <script src="assets/js/plugin/price_range_script.js"></script>
+    <script src="assets/js/plugin/in-number.js"></script>
+    <script src="assets/js/plugin/jquery.elevateZoom-3.0.8.min.js"></script>
+    <script src="assets/js/plugin/venobox.min.js"></script>
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <!-- <script src="assets/js/vendor/vendor.min.js"></script>
+    <script src="assets/js/plugin/plugins.min.js"></script> -->
+
+    <!-- Main js file that contents all jQuery plugins activation. -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
