e4263c9e7d463b9ed51fa934a5c61661336c4a82
omajork <126159210+omajork@users.noreply.github.com>
2023-02-23 16:16:53 +0100

    Create tables.php

---
+++ php/tables.php
@@ -0,0 +1,26 @@
+<?php
+
+    //Table Name definitions
+    define ("CART","cart");
+    define ("CATEGORY","category");
+    define ("CUSTOMERS","customers");
+
+    define ("DETAIL","order_detail");
+    define ("ORDERS","order_tb");
+
+    define ("PAYMENT","payment");
+    define ("PRODUCT","product");
+    define ("PRODIMAGE","product_image");
+    define ("RATINGS","ratings");
+
+    define ("SHIPMENT","shipment");
+    define ("USERS","users");
+    define ("SLIDES","slides");
+    define ("DISCOUNT", "discount");
+    define ("WISH", "wishlist");
+    define ("BLOG", "blogs");
+    define ("BLOGCAT", "blog_cat");
+    define ("COMMENTS", "blog_comment");
+    define ("DELIVERYRATING", 'deliveries');
+
+?>
