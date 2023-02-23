28849aa510fe15ec77902b92788887a2f744888d
omajork <126159210+omajork@users.noreply.github.com>
2023-02-23 14:14:36 +0100

    Create success.php

---
+++ success.php
@@ -0,0 +1,95 @@
+<?php
+    include './php/funct.php';
+    $fn = new funct();
+    $checkout = (isset($_GET['out'])) ? "true" : "";
+
+?>
+<!DOCTYPE html>
+<html lang="en-US" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+         <title>Abamade &#8211; Abamade Ecommerce Platform</title>
+        <meta name="description" content="Made by Nigerians for Nigerians and the World.">
+        <meta name="author" content="TACOEE CONSULTS LIMITED - https://tacoee.com">
+
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="assets/css/animate.min.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="assets/css/font-electro.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="assets/css/owl-carousel.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" media="all" />
+        <link rel="stylesheet" type="text/css" href="assets/css/colors/yellow.css" media="all" />
+
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,700italic,800,800italic,600italic,400italic,300italic' rel='stylesheet' type='text/css'>
+
+        <link rel="shortcut icon" href="assets/images/fav-icon.png">
+    </head>
+
+    <body class="page home page-template-default">
+        <div id="page" class="hfeed site">
+            <a class="skip-link screen-reader-text" href="#site-navigation">Skip to navigation</a>
+            <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+
+           <?php 
+
+                include_once 'top.php'; 
+                include_once 'main-head.php';
+                include_once 'main-nav.php'; 
+            ?>
+
+            <div id="content" class="site-content" tabindex="-1">
+                <div class="container">
+
+                    <nav class="woocommerce-breadcrumb" ><a href="index.php">Home</a><span class="delimiter"><i class="fa fa-angle-right"></i></span>My Account</nav><!-- .woocommerce-breadcrumb -->
+
+                    <div id="primary" class="content-area">
+                        <main id="main" class="site-main">
+                            <article id="post-8" class="hentry">
+
+                                <div class="entry-content">
+                                    <div class="woocommerce">
+                                        <div class="customer-login-form">
+
+                                            <div class="text-content">
+            									<h2 class="align-top">Successful Order with No: <?=strtoupper($_SESSION['orderNO']);?></h2>
+            									<p>
+                                                    Thank you <?= $_SESSION['customer_name'];?>, your order has being placed successfully. <br />
+                                                    Your ordered items will be shipped to @ <?= $_SESSION['shipDetail']->address.'  '.$_SESSION['shipDetail']->city.' '.$_SESSION['shipDetail']->state; ?>
+            									</p>
+
+            								</div>
+
+                                        </div><!-- /.customer-login-form -->
+                                    </div><!-- .woocommerce -->
+                                </div><!-- .entry-content -->
+
+                            </article><!-- #post-## -->
+
+                        </main><!-- #main -->
+                    </div><!-- #primary -->
+
+                </div><!-- .col-full -->
+            </div><!-- #content -->
+             <?php 
+
+                include_once 'brands.php'; 
+                include_once 'foot2.php'; 
+
+            ?>
+        </div><!-- #page -->
+
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/tether.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+        <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="assets/js/echo.min.js"></script>
+        <script type="text/javascript" src="assets/js/wow.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.easing.min.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.waypoints.min.js"></script>
+        <script type="text/javascript" src="assets/js/electro.js"></script>
+
+    </body>
+</html>
